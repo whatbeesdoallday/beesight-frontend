3d6f9a80b1b39f49de3dc347474328a7be445e82
whatbeesdoallday <gordon.hazle@gmail.com>
2025-06-15 17:07:36 +0100

    Update tailwind.config.ts

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: [
   './index.html',
-  './src/**/*.{js,ts,jsx,tsx}',
+  './src/**/*.{js,ts,jsx,tsx,css}',
 ],
 
   theme: {
