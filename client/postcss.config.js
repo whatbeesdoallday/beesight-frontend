4d7990808c8a4fde81bc6428287c6dd3d8e4345d
whatbeesdoallday <gordon.hazle@gmail.com>
2025-06-15 12:22:01 +0100

    Update postcss.config.js

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,6 +1,6 @@
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
 export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
+  plugins: [tailwindcss, autoprefixer],
 };
